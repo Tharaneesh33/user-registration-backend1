c4aff07a1ca22de053ba93425f3e8b80a43b989b
Tharaneesh Raj <tharaneeshsami@gmail.com>
2025-03-09 21:06:12 +0530

    Update userroutes.js

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -35,6 +35,17 @@ router.get("/", async (req, res) => {
   }
 });
 
+//Read Single User
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ error: "User not found" });
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 // Update User
 router.put("/:id", async (req, res) => {
   try {
