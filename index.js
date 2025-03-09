2b68fcb78eb09bbca69e406b12d9237a2109a744
Tharaneesh Raj <tharaneeshsami@gmail.com>
2025-03-10 02:22:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 dotenv.config(); // Load environment variables
 
@@ -10,12 +12,21 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // Middleware for JSON data
 
+function swaggerDocumentation() {
+  /* Documentation */
+  const swaggerSpec = YAML.load('./openapi.yml');
+  if (swaggerSpec.servers === undefined || swaggerSpec.servers === null) {
+      swaggerSpec.servers = [];
+  }
+  return swaggerSpec;
+}
+
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("MongoDB Connection Error:", err));
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocumentation()));
 app.use("/api/users", require("./routes/userroutes")); // User routes
 
 const PORT = process.env.PORT || 5000;
